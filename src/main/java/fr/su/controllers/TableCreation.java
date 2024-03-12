9f426b74de18c08d5b8242bbe41f329a3b1e1257
William Rosewarn <william.rosewarn@axians.com>
2024-03-12 15:17:40 +0100

    📝 First ideas for JSON body structure

--- src/main/java/fr/su/controllers/TableCreation.java
+++ src/main/java/fr/su/controllers/TableCreation.java
@@ -2,4 +2,34 @@ package fr.su.controllers;
 
 public class TableCreation {
 
+    /**
+     * Exemple de body
+     * POST
+     *
+     * {
+     *     "tableName":"test",
+     *     "columns": [
+     *
+     *          {
+     *              "columnName": "nom",
+     *              "type": "STRING"
+     *
+     *          },
+     *
+     *          {
+     *              "columnName": "prenom",
+     *              "type": "STRING"
+     *          },
+     *
+     *          {
+     *              "columnName": "age",
+     *              "type": "INTEGER"
+     *          }
+     *     ]
+     *
+     *
+     *
+     * }
+     */
+
 }   
--- src/main/java/fr/su/controllers/TableSelection.java
+++ src/main/java/fr/su/controllers/TableSelection.java
@@ -1,4 +1,22 @@
 package fr.su.controllers;
 
 public class TableSelection {
+
+    /**
+     * GET
+     * {
+     *     "columns": ["prenom", "age"],
+     *     "table": "test",
+     *     "where": [
+     *
+     *          {
+     *              "left": "prenom",
+     *              "operation": "eq",
+     *              "right": "Jean"
+     *          }
+     *      ]
+     * }
+     *
+     * Comment faire des jointures, des ordres dans les AND et OR dans le where ?
+     */
 }
