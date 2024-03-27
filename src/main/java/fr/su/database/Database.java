f54ee74122ea0497281d925fe69b3d21bebe3ee4
Tsouli-Abde <93455423+Tsouli-Abde@users.noreply.github.com>
2024-03-27 15:21:18 +0100

    Database Singleton

--- src/main/java/fr/su/database/Database.java
+++ src/main/java/fr/su/database/Database.java
@@ -1,5 +1,6 @@
 package fr.su.database;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,9 +11,36 @@ public class Database {
      *
      * Dans une table il y a des colonnes
      * Dans des colonnes il y a des variables
-     */
+     *
 
     private String name;
     private UUID uuid;
+     */
+
+    private static class Instance{
+        private static final Database INSTANCE = new Database();
+        //Meme démarche vu en cours
+        // pour garantir qu'une seule instance de la base de données est créée
+        // même dans un environnement multi-threadé.
+    }
+
     private List<Table> tables;
+
+    private Database() {
+        this.tables = new ArrayList<>();
+    }
+
+    public static Database getInstance() {
+        return Instance.INSTANCE;
+    }
+
+    public List<Table> getTables() {
+        return new ArrayList<>(tables);
+    }
+
+    public Table getTableByIndex(int i) {
+        //utilité pas encore claire mais
+        return tables.get(i);
+    }
+
 }
