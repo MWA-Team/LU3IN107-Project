44659c11f44940985d9ebe5edd7c92f28ec865fb
Faioa <matthieu.pcf.dartois@gmail.com>
2024-04-13 12:43:16 +0200

    Managing Responses from Forwarded Queries

--- src/main/java/fr/su/controllers/TableInsertion.java
+++ src/main/java/fr/su/controllers/TableInsertion.java
@@ -4,8 +4,10 @@ import fr.su.handlers.ForwardingManager;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.io.IOException;
+import java.util.List;
 
 @Path("/insert")
 public class TableInsertion {
@@ -15,8 +17,11 @@ public class TableInsertion {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Object getFile(String body) throws IOException {
-        return forwardingManager.forwardPost(body);
+    public Response getFile(String body) throws IOException {
+        Response response = Response.status(200).entity("This is fine").build();
+        Response forward = forwardingManager.forwardPost(body);
+        // If forward is null, the query was not forwarded, else check the status code and deal with it
+        return forward == null ? response : forward;
     }
 
 }
