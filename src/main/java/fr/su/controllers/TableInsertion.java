dad3ff963e24eeb0f459b5de0dba6c2dc1326605
Matthieu.D <111203877+Faioa@users.noreply.github.com>
2024-04-15 20:25:13 +0200

    Managing Responses from Forwarded Queries (#6)
    Co-authored-by: Faioa <matthieu.pcf.dartois@gmail.com>

--- src/main/java/fr/su/controllers/TableInsertion.java
+++ src/main/java/fr/su/controllers/TableInsertion.java
@@ -4,8 +4,10 @@ import fr.su.handlers.ForwardingManager;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.io.IOException;
+import java.util.List;
 
 @Path("/insert")
 public class TableInsertion {
@@ -15,7 +17,7 @@ public class TableInsertion {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Object getFile(String body) throws IOException {
+    public Response getFile(String body) throws IOException {
 
         /**
          *
@@ -34,8 +36,10 @@ public class TableInsertion {
          *
          *
          */
-
-        return forwardingManager.forwardPost(body);
+        Response response = Response.status(200).entity("This is fine").build();
+        Response forward = forwardingManager.forwardPost(body);
+        // If forward is null, the query was not forwarded, else check the status code and deal with it
+        return forward == null ? response : forward;
     }
 
 }
--- src/main/java/fr/su/proxy/ProxyLambda.java
+++ src/main/java/fr/su/proxy/ProxyLambda.java
@@ -2,12 +2,13 @@ package fr.su.proxy;
 
 import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
 
 import java.util.concurrent.Callable;
 
 @FunctionalInterface
 public interface ProxyLambda {
 
-    Object call(ForwardingProxy proxy, @HeaderParam("Server-Signature") String signature, @QueryParam("server_id") String id, String body);
+    Response call(ForwardingProxy proxy, @HeaderParam("Server-Signature") String signature, @QueryParam("server_id") String id, String body);
 
 }
