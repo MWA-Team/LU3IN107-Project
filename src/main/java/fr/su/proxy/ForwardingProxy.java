cbc4e18c5222f268f06cd65b2e786dc2e5e776e2
Faioa <matthieu.pcf.dartois@gmail.com>
2024-04-25 22:47:39 +0200

    🚧 Testing

--- src/main/java/fr/su/proxy/ForwardingProxy.java
+++ src/main/java/fr/su/proxy/ForwardingProxy.java
@@ -16,6 +16,7 @@ public interface ForwardingProxy {
     Response insert(@HeaderParam ("Server-Signature") String signature, @QueryParam("server_id") String id, File body);
 
     @GET
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     Response select(@HeaderParam ("Server-Signature") String signature, @QueryParam("server_id") String id, String body);
 
