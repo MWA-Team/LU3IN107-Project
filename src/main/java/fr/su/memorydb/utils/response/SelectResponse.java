7c49aca56162c3b15f65faea377c4fd113e71c4f
Faioa <matthieu.pcf.dartois@gmail.com>
2024-06-03 15:21:12 +0200

    ✨ Adding more information in the SelectResponse body

--- src/main/java/fr/su/memorydb/utils/response/SelectResponse.java
+++ src/main/java/fr/su/memorydb/utils/response/SelectResponse.java
@@ -1,12 +1,20 @@
 package fr.su.memorydb.utils.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import fr.su.memorydb.controllers.TableSelection;
+import fr.su.memorydb.database.Database;
 import jakarta.ws.rs.core.Link;
 
 import java.util.*;
 
 public class SelectResponse extends ValidResponse {
+
+    @JsonProperty
+    private int nbRowsReturned;
+
+    @JsonProperty
+    private int nbRowsParsed;
 
     @JsonProperty
     private List<HashMap<String, Object>> rows;
@@ -16,6 +24,8 @@ public class SelectResponse extends ValidResponse {
     public SelectResponse(String tableName, List<HashMap<String, Object>> rows) {
         super(tableName);
         this.rows = rows;
+        nbRowsReturned = rows.size();
+        nbRowsParsed = Database.getInstance().getTables().get(tableName).getRowsCounter();
     }
 
     public List<HashMap<String, Object>> getRows() {
